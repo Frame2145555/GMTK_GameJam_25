945abbb07c480bab8859134f1e6a829681ae8e47
ErrkkSkyseer <lolplayer78876@gmail.com>
2025-07-07 21:19:43 +0700

    Quest and Unit

---
+++ Assets/Script/Immegration Game Play/Unit.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+[System.Serializable]
+
+public enum UnitGrade
+{
+    G,F,E,D,C,B,A,S,SS,SSS
+}
+[System.Serializable]
+
+public enum UnitJob
+{
+    Archer,
+    Warrior,
+    Mage
+}
+[System.Serializable]
+
+public struct UnitInfo
+{
+    public string name;
+    public UnitGrade grade;
+    public UnitJob job;
+}
+/// <summary>
+/// Unit class is the data container of a person
+/// </summary>
+[System.Serializable]
+public class Unit
+{
+    private UnitInfo m_info;
+    private bool m_accept;
+    private bool m_reject;
+
+    public Unit(UnitInfo info)
+    {
+        m_info = info;
+    }
+
+    public UnitInfo Info { get => m_info;}
+    public bool Accept { get => m_accept; set => m_accept = value; }
+    public bool Reject { get => m_reject; set => m_reject = value; }
+
+}
