a53cd648b24dc46718fcdb6fc684dd2734a8da3e
_MeePooh_ <122891690+PluemDontKnowToCode@users.noreply.github.com>
2025-07-07 22:47:45 +0700

    ✨ feat: Add CreateRandomInfo()

--- Assets/Script/Immegration Game Play/ImmigrationManager.cs
+++ Assets/Script/Immegration Game Play/ImmigrationManager.cs
@@ -1,24 +1,46 @@
 using UnityEngine;
 using System.Collections.Generic;
-
+using System;
 public class ImmigrationManager : MonoBehaviour
 {
     [SerializeField] private Quest m_todayQuest;
-    [SerializeField] private Queue<Unit> m_todayUnits;
+    [SerializeField] private Queue<Unit> m_todayUnits = new Queue<Unit>();
 
     [SerializeField] private Unit m_currentUnit;
-    [SerializeField] private List<Unit> m_choosenUnit;
+    [SerializeField] private List<Unit> m_choosenUnit = new List<Unit>();
+    public UnitInfo CreateRandomInfo()
+    {
+        string[] randomName = { "Pooh", "Paul", "Sky", "Shogun", "Showy", "Kong" };
 
+        UnitInfo unitInfo = new UnitInfo();
+        unitInfo.name = randomName[UnityEngine.Random.Range(0, randomName.Length)];
+
+        unitInfo.grade = (UnitGrade)UnityEngine.Random.Range(0, System.Enum.GetValues(typeof(UnitGrade)).Length);
+        unitInfo.job = (UnitJob)UnityEngine.Random.Range(0, System.Enum.GetValues(typeof(UnitJob)).Length);
+        //write a code that randomized unitInfo struct.
+        Debug.Log($"Create UnitInfo Name: {unitInfo.name}, Grade: {unitInfo.grade}, Job: {unitInfo.job}");
+        return unitInfo;
+    }
     public void GenerateQuest()
     {
         Quest m_quest = new Quest(name);
+
+        m_quest.addUnitRequirment(CreateRandomInfo(), UnityEngine.Random.Range(1, 3));
+
+
         m_todayQuest = m_quest;
     }
     public void GenerateUnits()
     {
-
+        UnitInfo m_unitInfo = CreateRandomInfo();
+        Unit m_unit = new Unit(m_unitInfo);
+        m_todayUnits.Enqueue(m_unit);
     }
-
+    public void GenerateUnits(UnitInfo unitInfo)
+    {
+        Unit m_unit = new Unit(unitInfo);
+        m_todayUnits.Enqueue(m_unit);
+    }
     public void nextDay()
     {
 
@@ -31,12 +53,14 @@ public class ImmigrationManager : MonoBehaviour
 
     public void accept()
     {
+        m_currentUnit.Accept = true;
         m_choosenUnit.Add(m_currentUnit);
         nextPerson();
     }
 
     public void reject()
     {
+        m_currentUnit.Reject = false;
         nextPerson();
     }
 }
